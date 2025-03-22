f532067a7d9c72e970d9a6a68a7cec4df9934fa8
ph4 <22990490+ph4@users.noreply.github.com>
2025-03-22 18:24:14 +0300

    Refactor main logic into it's own class

---
+++ src/Recorder.ixx
@@ -0,0 +1,57 @@
+//
+// Created by pavel on 22.03.2025.
+//
+module;
+#include <memory>
+#include <unordered_set>
+#include <Controller.hpp>
+
+export module Recorder;
+import Api;
+import Models;
+import FileUploader;
+import ProcessRecorder;
+import ProcessLister;
+import WinAudioSource;
+import AudioSource;
+
+
+struct RecorderItem {
+    std::unique_ptr<recorder::ProcessRecorder<int16_t>> recorder;
+    recorder::ProcessInfo process;
+};
+
+namespace recorder {
+    export class Recorder {
+         std::shared_ptr<models::LocalConfig> config_{};
+        models::RemoteConfig remote_config_{};
+        std::shared_ptr<Api> api_{};
+        std::shared_ptr<FileUploader> uploader_{};
+        std::shared_ptr<Controller> controller_{};
+        ProcessLister process_lister_{};
+        std::unordered_map<std::string, std::unique_ptr<RecorderItem>> recorders_{};
+        std::unordered_set<std::string> app_whitelist_{};
+
+
+        void LoadConfig();
+
+        void Register();
+
+        void WaitAllStopped();
+
+        void StartRecordingOnProcess(const ProcessInfo &pi);
+
+        void AddNewProcesses();
+
+        void RemoveStoppedProcesses();
+
+        bool ListenProcesses();
+
+        void Init();
+
+    public:
+        Recorder() = default;
+
+        void MainLoop();
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,178 +7,17 @@
 
 // Force include before Windows so it does not complain
 #include <httplib.h>
+#include <windows.h>
 
-// Workaround unresolved external symbol
-#include <wrl.h>
-
-#include <shlobj_core.h>
-
-#include <yyjson.h>
-
-
-#include <controller.hpp>
-#include <ranges>
-
-#include <rfl.hpp>
-#include <rfl/Variant.hpp>
-#include <rfl/Timestamp.hpp>
-#include <rfl/toml/load.hpp>
-#include <rfl/toml/save.hpp>
-
+#include <filesystem>
 
 #include "hwid.hpp"
 #include "logging.hpp"
 
-
-import AudioSource;
-import WinAudioSource;
-import ProcessRecorder;
-import Models;
-import Api;
-import FileUploader;
-import ProcessLister;
 import Velopack;
+import Recorder;
 
-using recorder::models::LocalConfig;
-
-using namespace std::chrono;
-namespace rv = std::ranges::views;
-
-#define FORMAT sizeof(uint16_t)
-
-steady_clock::time_point last_update_time{};
-
-
-struct RecorderItem {
-    std::unique_ptr<recorder::ProcessRecorder<int16_t>> recorder;
-    recorder::ProcessInfo process;
-};
-
-std::shared_ptr<LocalConfig> local_config;
 std::string app_path{"."};
-
-int start() {
-    auto config_path = app_path + "\\config.toml";
-    auto config_load = rfl::toml::load<LocalConfig>(config_path).and_then([](auto config) {
-        return rfl::Result(std::make_shared<LocalConfig>(std::move(config)));
-    });
-    if (!config_load) {
-        auto a = config_load.error().value();
-        SPDLOG_ERROR("Error reading config ({})", config_load.error().value().what());
-        return 1;
-    }
-    local_config = config_load.value();
-    auto api = std::make_shared<recorder::Api>(local_config);
-    auto res = api->Authorize().or_else([](auto e) {
-        SPDLOG_ERROR("Error authorizing API ({})", e.what());
-        return e;
-    });
-
-
-    auto api_a = api->EnsureAuthorized();
-
-    auto set_name_r = api->SetName();
-    if (set_name_r.error()) {
-        SPDLOG_ERROR("Error setting name {}", set_name_r.error().value().what());
-    }
-
-    auto remote_cfg = api->GetConfig();
-    auto using_cached_remote_config = false;
-    auto remote_config_r = remote_cfg.or_else([&](auto e) {
-        SPDLOG_WARN("Using cached remote config");
-        using_cached_remote_config = true;
-        return rfl::toml::load<recorder::models::RemoteConfig>("remote_config.toml");
-    });
-    if (!remote_config_r) {
-        SPDLOG_ERROR("Error loading remote config");
-        return 1;
-    }
-    const auto remote_config = remote_config_r.value();
-    if (!using_cached_remote_config) {
-        rfl::toml::save<>("remote_config.toml", remote_config);
-    }
-
-    const auto uploader = std::make_shared<recorder::FileUploader>(api, std::filesystem::path(app_path + "/records/"));
-    const auto controller = std::make_shared<recorder::Controller>(api, 5000);
-    // controller->SetStatus("main", recorder::InternalStatusBase{.type = recorder::InternalStatusType::idle});
-
-    auto audio_format = recorder::audio::AudioFormat{
-            .channels = 1,
-            .sampleRate = 16000,
-    };
-
-    recorder::ProcessLister pl;
-    std::unordered_map<std::string, std::unique_ptr<RecorderItem>> recorders;
-    while (true) {
-        auto start = high_resolution_clock::now();
-        switch (auto cmd = controller->GetGlobalCommand().type) {
-            using enum recorder::models::CommandType;
-            case reload:
-            case stop:
-            case kill: {
-                bool all_stopped = true;
-                for (auto &recorder: recorders | std::views::values) {
-                    auto &rec = recorder->recorder;
-                    if (!rec->IsStopped() && rec->IsStarted()) {
-                        all_stopped = false;
-                    }
-                }
-                if (all_stopped) {
-                    if (cmd == reload) {
-                        return 0x0EADBEEF;
-                    } else {
-                        return 0;
-                    }
-                } else {
-                    auto elapsed = high_resolution_clock::now() - start;
-                    auto to_sleep = milliseconds(200) - elapsed;
-                    std::this_thread::sleep_for(to_sleep);
-                    continue;
-                }
-                break;
-            }
-            default:
-                break;
-        }
-
-        auto playing_processes = pl.getAudioPlayingProcesses();
-        std::unordered_set<std::string> whitelist;
-        auto wl = remote_config.app_configs | rv::transform([](recorder::models::App app) { return app.exe_name; });
-        for (auto e: wl) {
-            whitelist.insert(e);
-        }
-        auto new_processes = playing_processes | rv::filter([&](recorder::ProcessInfo e) {
-                                 return whitelist.contains(e.process_name()) && !recorders.contains(e.process_name());
-                             });
-        auto factory = [](auto fmt, auto cb, auto scb, auto pid, auto lb) {
-            return std::make_unique<recorder::audio::windows::WinAudioSource<int16_t>>(fmt, cb, scb, pid, lb);
-        };
-        for (const auto &p: new_processes) {
-            auto recorder = std::make_unique<recorder::ProcessRecorder<int16_t>>(
-                    controller, p.process_name(), uploader, audio_format, p.process_id(), factory);
-            SPDLOG_INFO("Starting recording on {}", p.process_name());
-            recorder->Play();
-            auto ri = std::make_unique<RecorderItem>(std::move(recorder), p);
-            recorders.emplace(p.process_name(), std::move(ri));
-        }
-        auto to_remove = std::vector<std::string>();
-        for (const auto &[k, v]: recorders) {
-            if (!v->process.isAlive()) {
-                SPDLOG_INFO("Stopping recording on {}", v->process.process_name());
-                v->recorder->Stop();
-                to_remove.push_back(k);
-            }
-        }
-        for (const auto &k: to_remove) {
-            recorders.erase(k);
-        }
-        auto elapsed = high_resolution_clock::now() - start;
-        auto to_sleep = milliseconds(200) - elapsed;
-        std::this_thread::sleep_for(to_sleep);
-    }
-stop_loop: {}
-}
-
 
 void SetWorkdirToParent() {
     // Get the full path to the running executable
@@ -204,24 +43,24 @@ int main(const int argc, char const *argv[]) {
 #endif
     ShowWindow(GetConsoleWindow(), SW_HIDE); // Hide terminal
     setup_logger();
-    if (const auto res = recorder::velopack::init_velopack()) {
-        return res;
-    };
+    try {
+        if (const auto res = recorder::velopack::init_velopack()) {
+            return res;
+        };
+
+    } catch (const std::exception &e) {
+        SPDLOG_ERROR("Exception occurred while starting velopack: {}", e.what());
+    }
 
     auto uuid = get_uuid();
     SPDLOG_INFO("Machine UUID = {}", uuid);
 
+    auto recorder = recorder::Recorder();
     std::thread recorder_thread([&] {
-        while (true) {
-            const auto res = start();
-            if (res == 0x0EADBEEF) {
-                SPDLOG_INFO("Reloading");
-                // RELOAD
-            } else if (res != 0) {
-                std::exit(res);
-            } else {
-                break;
-            }
+        try {
+            recorder.MainLoop();
+        } catch (const std::exception &e) {
+            SPDLOG_ERROR("Exception: {}", e.what());
         }
     });
 
