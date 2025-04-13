9f871bcea8e3c26b723bee1ed7892db60e3d1d05
ph4 <22990490+ph4@users.noreply.github.com>
2025-04-13 23:38:08 +0300

    logging in logs dir

--- src/logging.cpp
+++ src/logging.cpp
@@ -4,7 +4,7 @@
 
 void setup_logger() {
     auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log.txt", 1024 * 1024 * 5, 5, true);
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/main.txt", 1024 * 1024 * 5, 5, true);
     spdlog::logger logger("main", {stdout_sink, file_sink});
     logger.flush_on(spdlog::level::trace);
     logger.set_pattern("[%Y-%m-%d %H:%M:%S.%f] [%s] [%^%l%$] %v");
